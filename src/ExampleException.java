544a6a4282674069596d86ef744ef2b154454658
menvol3 <disbalance@ukr.net>
2020-04-05 18:48:59 +0300

    third

--- src/ExampleException.java
+++ src/ExampleException.java
@@ -1,15 +1,9 @@
 public class ExampleException {
-  public static int rectangleArea(int a , int b){
-   int area = a * b;
-   if (a < 0 | b < 0){
-     try {
-         throw new IllegalArgumentException();
-     } catch (IllegalArgumentException ex){
-           System.out.println("input value is below zero!");
-     }
-   }
-    return area;
-  }
+    public static int rectangleArea(int a , int b) throws IllegalArgumentException{
+        int area = a * b;
+        if (a < 0 | b < 0){
+                throw new IllegalArgumentException("input value is below zero!");
+            }
+        return area;
+    }
 }
-
-
--- src/ExampleExceptionTest.java
+++ src/ExampleExceptionTest.java
@@ -6,45 +6,48 @@ import org.testng.annotations.Test;
 
 public class ExampleExceptionTest {
 
-  /**
-   * Test data for positive test.
-   *
-   * @return test data array
-   */
-  @DataProvider(name = "data")
-  public static Object[][] data() {
-    return new Object[][]{
-        {4, 8, 32},
-        {5, 10, 50},
-        {2, 2, 4},
-        {2, 3, 6}
-    };
-  }
-
-  /**
-   * Test data for exception test.
-   *
-   * @return test data array
-   */
-  @DataProvider(name = "negativeData")
-  public static Object[][] negativeData() {
-    return new Object[][]{
-        {-2, 2},
-        {2, -2}
-        // TODO add 2 more test data here
-    };
-  }
-
-  @Test(dataProvider = "data")
-  public static void testRectangleArea(int a, int b, int c) {
-    int area =  ExampleException.rectangleArea(a, b);
-    assertEquals(area, c, "Area is not correct");
-  }
-
-
-  @Test(dataProvider = "negativeData")
-  public void testRectangleAreaNegative(int a, int b)  {
-    int actualResult = ExampleException.rectangleArea(a, b);
-    throw new IllegalArgumentException ("input value is below zero!");
+    /**
+     * Test data for positive test.
+     *
+     * @return test data array
+     */
+    @DataProvider(name = "data")
+    public static Object[][] data() {
+        return new Object[][]{
+                {4, 8, 32},
+                {5, 10, 50},
+                {2, 2, 4},
+                {2, 3, 6}
+        };
     }
-  }
+
+    /**
+     * Test data for exception test.
+     *
+     * @return test data array
+     */
+    @DataProvider(name = "negativeData")
+    public static Object[][] negativeData() {
+        return new Object[][]{
+                {-2, 2},
+                {2, -2}
+                // TODO add 2 more test data here
+        };
+    }
+
+    @Test(dataProvider = "data")
+    public static void testRectangleArea(int a, int b, int c) {
+        int area = ExampleException.rectangleArea(a, b);
+        assertEquals(area, c, "Area is not correct");
+    }
+
+
+    @Test(dataProvider = "negativeData")
+    public void testRectangleAreaNegative(int a, int b) {
+        try {
+            ExampleException.rectangleArea(a, b);
+            fail("No exception thrown!");
+        } catch (IllegalArgumentException e) {
+        }
+    }
+}
